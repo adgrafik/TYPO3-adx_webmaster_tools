9a7a0e73721980b63fef7d9c06674dc43404c850
Arno Dudek <arno.dudek@maas.at>
2016-02-24 10:01:34 +0100

    [BUGFIX] Added missing rootline fields.

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,7 +7,7 @@ $extensionConfiguration = unserialize($TYPO3_CONF_VARS['EXT']['extConf']['adx_we
 $TYPO3_CONF_VARS['FE']['addRootLineFields'] .= ',subtitle,description,abstract,keywords,author';
 $TYPO3_CONF_VARS['FE']['addRootLineFields'] .= ',tx_adxwebmastertools_verification_google_id,tx_adxwebmastertools_verification_yahoo_id';
 $TYPO3_CONF_VARS['FE']['addRootLineFields'] .= ',tx_adxwebmastertools_xml_sitemap_disable,tx_adxwebmastertools_xml_sitemap_priority,tx_adxwebmastertools_xml_sitemap_changefrequence';
-$TYPO3_CONF_VARS['FE']['addRootLineFields'] .= ',tx_adxwebmastertools_google_analytics_account_id,tx_adxwebmastertools_google_analytics_domain_name,tx_adxwebmastertools_google_analytics_allow_linker,tx_adxwebmastertools_google_analytics_local_remote_server_mode';
+$TYPO3_CONF_VARS['FE']['addRootLineFields'] .= ',tx_adxwebmastertools_google_analytics_account_id,tx_adxwebmastertools_google_analytics_domain_name,tx_adxwebmastertools_google_analytics_anonymize_ip,tx_adxwebmastertools_google_analytics_allow_linker,tx_adxwebmastertools_google_analytics_allow_hash,tx_adxwebmastertools_google_analytics_local_remote_server_mode';
 
 // add page types to real url
 $TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['fileName']['index']['rss.xml']['keyValues']['type'] = 1341932124;
